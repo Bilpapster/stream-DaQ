3f0f10fc5e2a0948e303ac039b21fe71e0e5a556
Vasileios Papastergios <papastva@csd.auth.gr>
2024-06-08 23:38:31 +0300

    Add example for counting page views

---
+++ examples/page_views.py
@@ -0,0 +1,23 @@
+import faust
+
+app = faust.App(
+    'page_views',
+    broker='kafka://localhost:9092',
+    topic_partitions=4,
+)
+
+
+class PageView(faust.Record):
+    id: str
+    user: str
+
+
+page_view_topic = app.topic('page_views', value_type=PageView)
+page_views = app.Table('page_views', default=int)
+
+
+@app.agent(page_view_topic)
+async def count_page_views(views):
+    async for view in views.group_by(PageView.id):
+        page_views[view.id] += 1
+        print(f'Received a page view for page {view.id} from user {view.user}')
