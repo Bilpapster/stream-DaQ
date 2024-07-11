9cd1952409e1b45307a26d313ffed934bf4c05bd
Vasileios Papastergios <papastva@csd.auth.gr>
2024-07-11 10:46:07 +0300

    Update gif to have frames in correct temporal order

--- dq_dashboard/gif_maker.py
+++ dq_dashboard/gif_maker.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 
 def make_gif(frame_folder):
-    frames = [Image.open(image) for image in glob.glob(f"{frame_folder}/*.png")]
+    frames = [Image.open(f"{frame_folder}/Figure_{number}.png") for number in range(1, 11)]
     frame_one = frames[0]
     frame_one.save("dq_dashboard.gif", format="GIF", append_images=frames, save_all=True, duration=1100, loop=0)
 
