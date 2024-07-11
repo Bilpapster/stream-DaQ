266a609658b54355c6f3470dbac386b922f55e3f
Vasileios Papastergios <papastva@csd.auth.gr>
2024-07-11 11:24:43 +0300

    Update gif_maker function and feed new, more, fresher images

--- dq_dashboard/gif_maker.py
+++ dq_dashboard/gif_maker.py
@@ -5,9 +5,9 @@ from PIL import Image
 
 
 def make_gif(frame_folder):
-    frames = [Image.open(f"{frame_folder}/Figure_{number}.png") for number in range(1, 11)]
+    frames = [Image.open(f"{frame_folder}/Figure_{number}.png") for number in range(1, 50 + 1)]
     frame_one = frames[0]
-    frame_one.save("dq_dashboard.gif", format="GIF", append_images=frames, save_all=True, duration=1100, loop=0)
+    frame_one.save("dq_dashboard.gif", format="GIF", append_images=frames, save_all=True, duration=1500, loop=0)
 
 
 if __name__ == "__main__":
