ae3aaa89e9ccc2c0c2a6d68634ae2011f25fffa7
Vasileios Papastergios <papastva@csd.auth.gr>
2024-08-30 12:44:54 +0300

    Develop reducers for values above mean and distinct values

--- pathway/DaQMeasuresFactory.py
+++ pathway/DaQMeasuresFactory.py
@@ -71,3 +71,69 @@ class DaQMeasuresFactory:
         :return: a pathway sorted_tuple reducer
         """
         return pw.reducers.sorted_tuple(pw.this[column_name])
+
+    @staticmethod
+    def get_number_of_values_above_mean_reducer(column_name: str) -> pw.internals.expression.ColumnExpression:
+        """
+        Static getter to retrieve a custom reducer that computes the number of elements in the window that are greater
+        than the mean value of the window.
+        :param column_name: the column name of pw.this table to apply the reducer on
+        :return: a pathway pw.apply statement ready for use as a column
+        """
+        import numpy as np
+
+        def get_number_of_values_above_mean(numbers: list):
+            mean = np.mean(numbers)
+            return (numbers > mean).sum()
+
+        return pw.apply(get_number_of_values_above_mean, pw.reducers.ndarray(pw.this[column_name]))
+
+    @staticmethod
+    def get_fraction_of_values_above_mean_reducer(column_name: str,
+                                                  precision: int = 3) -> pw.internals.expression.ColumnExpression:
+        """
+        Static getter to retrieve a custom reducer that computes the fraction of elements in the window that are greater
+        than the mean value of the window. The fraction is in range [0, 1]
+        :param column_name: the column name of pw.this table to apply the reducer on
+        :param precision: the number of decimal points to include in the fraction result. Defaults to 3.
+        :return: a pathway pw.apply statement ready for use as a column
+        """
+        import numpy as np
+
+        def get_fraction_of_values_above_mean(numbers: list):
+            mean = np.mean(numbers)
+            fraction_above_mean = float((numbers > mean).sum() / len(numbers))
+            return round(fraction_above_mean, precision)
+
+        return pw.apply(get_fraction_of_values_above_mean, pw.reducers.ndarray(pw.this[column_name]))
+
+    @staticmethod
+    def get_number_of_distinct_values_reducer(column_name: str) -> pw.internals.expression.ColumnExpression:
+        """
+        Static getter to retrieve a custom reducer that computes the number of distinct elements in the window.
+        The fraction is in range [0, 1]
+        :param column_name: the column name of pw.this table to apply the reducer on
+        :return: a pathway pw.apply statement ready for use as a column
+        """
+
+        def get_number_of_distinct_values(numbers: list):
+            return len(set(numbers))
+
+        return pw.apply(get_number_of_distinct_values, pw.reducers.ndarray(pw.this[column_name]))
+
+    @staticmethod
+    def get_fraction_of_distinct_values_reducer(column_name: str,
+                                                precision: int = 3) -> pw.internals.expression.ColumnExpression:
+        """
+        Static getter to retrieve a custom reducer that computes the fraction of distinct elements in the window.
+        The fraction is in range [0, 1]
+        :param column_name: the column name of pw.this table to apply the reducer on
+        :param precision: the number of decimal points to include in the fraction result. Defaults to 3.
+        :return: a pathway pw.apply statement ready for use as a column
+        """
+
+        def get_fraction_of_distinct_values(numbers: list):
+            fraction = len(set(numbers)) / len(numbers)
+            return round(fraction, precision)
+
+        return pw.apply(get_fraction_of_distinct_values, pw.reducers.ndarray(pw.this[column_name]))
--- pathway/statistics_manager.py
+++ pathway/statistics_manager.py
@@ -1,5 +1,3 @@
-from xml.etree.ElementInclude import include
-
 import pathway as pw
 from datetime import datetime, timedelta
 
@@ -18,27 +16,30 @@ TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 WINDOW_DURATION_SEC = 5
 WAIT_FOR_DELAYED_SEC = 1
 
-
-data = (artificial_stream_generators.generate_artificial_random_viewership_data_stream(number_of_rows=10)
+data = (artificial_stream_generators.generate_artificial_random_viewership_data_stream(number_of_rows=100)
         .with_columns(date_and_time=pw.apply(convert_to_datetime, pw.this.timestamp))
         .with_columns(date_and_time=pw.this.date_and_time.dt.strptime(TIME_FORMAT)))
 
 data = data.windowby(
-        data.date_and_time,
-        # data.timestamp,
-        window=pw.temporal.tumbling(duration=timedelta(seconds=WINDOW_DURATION_SEC)),
-        # window=pw.temporal.tumbling(duration=WINDOW_DURATION_SEC),
-        instance=data.user_id,
-        behavior=pw.temporal.exactly_once_behavior(shift=timedelta(seconds=WAIT_FOR_DELAYED_SEC)),
+    data.date_and_time,
+    # data.timestamp,
+    window=pw.temporal.tumbling(duration=timedelta(seconds=WINDOW_DURATION_SEC)),
+    # window=pw.temporal.tumbling(duration=WINDOW_DURATION_SEC),
+    instance=data.user_id,
+    behavior=pw.temporal.exactly_once_behavior(shift=timedelta(seconds=WAIT_FOR_DELAYED_SEC)),
 ).reduce(
-        window_start = pw.this._pw_window_start,
-        window_end = pw.this._pw_window_end,
-        min_interactions = daq.get_min_reducer('interaction_events'),
-        max_interactions = daq.get_max_reducer('interaction_events'),
-        mean_interactions = daq.get_avg_reducer('interaction_events'),
-        count_interactions = daq.get_count_reducer('interaction_events'),
-        ndarray_interactions = daq.get_ndarray_reducer('interaction_events'),
-        tuple_interactions = daq.get_tuple_reducer('interaction_events'),
-        sorted_tuple_interactions = daq.get_sorted_tuple_reducer('interaction_events'),
+    window_start=pw.this._pw_window_start,
+    window_end=pw.this._pw_window_end,
+    # min=daq.get_min_reducer('interaction_events'),
+    # max=daq.get_max_reducer('interaction_events'),
+    mean=daq.get_avg_reducer('interaction_events'),
+    count=daq.get_count_reducer('interaction_events'),
+    ndarray=daq.get_ndarray_reducer('interaction_events'),
+    # tuple=daq.get_tuple_reducer('interaction_events'),
+    # sorted_tuple=daq.get_sorted_tuple_reducer('interaction_events'),
+    above_mean=daq.get_number_of_values_above_mean_reducer('interaction_events'),
+    above_mean_frac=daq.get_fraction_of_values_above_mean_reducer('interaction_events'),
+    distinct=daq.get_number_of_distinct_values_reducer('interaction_events'),
+    distinct_frac=daq.get_fraction_of_distinct_values_reducer('interaction_events'),
 )
 pw.debug.compute_and_print(data, include_id=False)
