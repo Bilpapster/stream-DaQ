2acc77f5008ec7260479b79ae1dd44ab85ace106
Vasileios Papastergios <papastva@csd.auth.gr>
2024-09-05 11:06:56 +0300

    Add a utility function for calculating the most frequent element and its frequency

--- pathway/utils/utils.py
+++ pathway/utils/utils.py
@@ -1,10 +1,15 @@
-def calculate_number_of_unique_values(elements: list) -> int:
-    import numpy as np
-
+def create_frequency_dict(elements: tuple) -> dict:
     frequency_dict = dict()
     for element in elements:
-        key = str(element)
+        key = element
         frequency_dict[key] = frequency_dict.get(key, 0) + 1
+    return frequency_dict
+
+
+def calculate_number_of_unique_values(elements: tuple) -> int:
+    import numpy as np
+
+    frequency_dict = create_frequency_dict(elements)
     frequencies = frequency_dict.values()
     number_of_unique_elements = np.sum(frequency == 1 for frequency in frequencies)
     return number_of_unique_elements
@@ -30,3 +35,19 @@ def calculate_number_of_range_conformance(elements: tuple, low: float, high: flo
 
 def calculate_number_of_set_conformance(elements: tuple, allowed_values: set):
     return sum(element in allowed_values for element in elements)
+
+
+def find_most_frequent_element(elements: tuple):
+    frequency_dict = create_frequency_dict(elements)
+    items = tuple(frequency_dict.items())
+    current_max = list(items[0])
+    items = items[1:] # to avoid iterating again over the first element
+    for item in items:
+        if item[1] > current_max[1]:
+            current_max = list(item)
+            continue
+        if item[1] == current_max[1]:
+            if type(current_max[0]) != list:
+                current_max[0] = [current_max[0]]
+            current_max[0].append(item[0])
+    return current_max # a tuple in the form (most_frequent_element, current_max_frequency)
