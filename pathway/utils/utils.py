af9e0a719a61baf58715f111bab237a351023264
Vasileios Papastergios <papastva@csd.auth.gr>
2024-09-09 16:18:45 +0300

    Add a utility function for sorting two arrays in parallel, based on timestamps

--- pathway/utils/utils.py
+++ pathway/utils/utils.py
@@ -41,7 +41,7 @@ def find_most_frequent_element(elements: tuple):
     frequency_dict = create_frequency_dict(elements)
     items = tuple(frequency_dict.items())
     current_max = list(items[0])
-    items = items[1:] # to avoid iterating again over the first element
+    items = items[1:]  # to avoid iterating again over the first element
     for item in items:
         if item[1] > current_max[1]:
             current_max = list(item)
@@ -50,4 +50,24 @@ def find_most_frequent_element(elements: tuple):
             if type(current_max[0]) != list:
                 current_max[0] = [current_max[0]]
             current_max[0].append(item[0])
-    return current_max # a tuple in the form (most_frequent_element, current_max_frequency)
+    return current_max  # a tuple in the form (most_frequent_element, current_max_frequency)
+
+
+def sort_by_timestamp(timestamps: list, elements: list[int | float | str], time_format: str) -> tuple:
+    """
+    Sorts timestamps and items in parallel, based on timestamps.
+    :param timestamps: a list of timestamps as floats
+    :param elements: a list of items as floats, corresponding to the timestamps
+    :param time_format: the time format in which the string values of the timestamp column are provided
+    :return: the list of items sorted, based on their timestamps
+    """
+    import numpy as np
+
+    # IMPORTANT: timestamps are objects of the pandas.Timestamp class (Pathway reads datetime in that way if you use the
+    # .dt utility, as presented in their tutorials, e.g.,
+    # https://pathway.com/developers/user-guide/temporal-data/windows-manual#windowby-reduce)
+    elements, timestamps = np.array(elements), np.array(timestamps)
+    timestamps_int = np.array([timestamp.value // 1e9 for timestamp in timestamps])
+    sorted_indices = np.argsort(timestamps_int)
+    timestamps_sorted, elements_sorted_by_timestamp = timestamps[sorted_indices], elements[sorted_indices]
+    return timestamps_sorted, elements_sorted_by_timestamp
