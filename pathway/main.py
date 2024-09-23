ee0dcf95b963aeb4405d89e6369c355437791c59
Vasileios Papastergios <papastva@csd.auth.gr>
2024-09-23 14:44:51 +0300

    Add monitoring configurations

--- pathway/main.py
+++ pathway/main.py
@@ -1,11 +1,20 @@
 from StreamDaQ import StreamDaQ
-from DaQMeasures import DaQMeasures as measures
+from DaQMeasures import DaQMeasures as dqm
+from Windows import tumbling
+from datetime import timedelta
 
-daq = StreamDaQ()
-daq.add(measures.count('items'), "count") \
-    .add(measures.min('items'), "min") \
-    .add(measures.max('items'), "max") \
-    .add(measures.median('items'), "std") \
-    .add(measures.most_frequent('items'), "max") \
-    .add(measures.number_of_distinct('items'), "distinct") \
-    .watch_out()
+daq = StreamDaQ().configure(
+    window=tumbling(duration=timedelta(hours=1)),
+    instance="user_id",
+    wait_for_late=timedelta(seconds=10),
+    time_format="%H:%M:%S"
+)
+
+daq.add(dqm.count('items'), "count") \
+    .add(dqm.min('items'), "min") \
+    .add(dqm.max('items'), "max") \
+    .add(dqm.median('items'), "std") \
+    .add(dqm.most_frequent('items'), "max") \
+    .add(dqm.number_of_distinct('items'), "distinct")
+
+daq.watch_out()
