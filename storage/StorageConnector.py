cee142ad82decb11271495d6fd9d28edcbe5807a
Vasileios Papastergios <papastva@csd.auth.gr>
2024-09-25 14:59:55 +0300

    Format file

--- storage/StorageConnector.py
+++ storage/StorageConnector.py
@@ -4,8 +4,8 @@ from abc import ABC, abstractmethod
 class StorageConnector(ABC):
     @abstractmethod
     def write(self, key, value):
-        pass
+        ...
 
     @abstractmethod
     def read(self, key):
-        pass
+        ...
