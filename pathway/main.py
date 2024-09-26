4b68543f096d791ff27ae484d8da74e2312afa3a
Vasileios Papastergios <papastva@csd.auth.gr>
2024-09-26 15:13:54 +0300

    Align driver program to minor API changes

--- pathway/main.py
+++ pathway/main.py
@@ -4,17 +4,19 @@ from Windows import tumbling
 from datetime import timedelta
 
 daq = StreamDaQ().configure(
-    window=tumbling(duration=timedelta(hours=1)),
+    window=tumbling(timedelta(seconds=20)),
     instance="user_id",
-    wait_for_late=timedelta(seconds=10),
-    time_format="%H:%M:%S"
+    time_column="timestamp",
+    wait_for_late=1,
+    time_format='%Y-%m-%d %H:%M:%S'
 )
 
 daq.add(dqm.count('interaction_events'), "count") \
     .add(dqm.min('interaction_events'), "min") \
     .add(dqm.max('interaction_events'), "max") \
-    .add(dqm.median('interaction_events'), "std") \
-    .add(dqm.most_frequent('interaction_events'), "max") \
+    .add(dqm.median('interaction_events'), "median") \
+    .add(dqm.most_frequent('interaction_events'), "frequent") \
+    .add(dqm.number_of_distinct_approx('interaction_events'), "distinct_approx") \
     .add(dqm.number_of_distinct('interaction_events'), "distinct")
 
 daq.watch_out()
