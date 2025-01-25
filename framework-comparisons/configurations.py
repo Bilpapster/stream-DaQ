4deaf0e1eb116bd52b8e84c9cb5cded5b82f743a
Vasileios Papastergios <papastva@csd.auth.gr>
2025-01-25 17:17:26 +0200

    Extract configurations into a yaml file for flexibility

--- framework-comparisons/configurations.py
+++ framework-comparisons/configurations.py
@@ -1,8 +1,9 @@
-def get_kafka_topic():
-    return 'test_topic'
+import yaml
 
-def get_number_of_elements_to_send():
-    return 100
+with open('configurations.yaml', 'r') as config_file:
+    configs = yaml.safe_load(config_file)
 
-def get_chance_of_missing_value():
-    return 0.1
+kafka_topic = configs.get('kafka_topic', 'default_topic')
+number_of_elements_to_send = configs.get('number_of_elements_to_send', 0)
+chance_of_missing_value = configs.get('chance_of_missing_value', 0.0)
+sleep_seconds_between_messages = configs.get('sleep_seconds_between_messages', 0.0)
