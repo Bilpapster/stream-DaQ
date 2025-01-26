71f20c28d4f5c45f15514755d46df17e57fb9a66
Vasileios Papastergios <papastva@csd.auth.gr>
2025-01-26 10:06:40 +0200

    Handle results with Table (fault tolerance) instead of plain variables

--- framework-comparisons/simple_faust.py
+++ framework-comparisons/simple_faust.py
@@ -9,6 +9,9 @@ from utils import is_out_of_range as range_validator, set_up_output_path, standa
 RESULTS_OUTPUT_FILE = directory_base_name + faust_directory + 'results.csv'
 set_up_output_path(RESULTS_OUTPUT_FILE)
 
+MISSING_KEYWORD = 'MISSING'
+OUT_OF_RANGE_KEYWORD = 'OUT_OF_RANGE'
+
 app = faust.App(
     'simple_dq',
     broker=kafka_server,
@@ -16,22 +19,22 @@ app = faust.App(
 
 
 @app.agent()
-async def write_to_csv(stream):
+async def write_to_csv(stream_of_timestamps):
     with open(RESULTS_OUTPUT_FILE, 'a') as csvfile:
-        async for value in stream:
+        async for timestamp in stream_of_timestamps:
             csvfile.write(
-                f'{value[0]},'  # current number of missing values
-                f'{value[1]},'  # current number of values out of range
-                f'{value[2]}'  # timestamp that the message was emitted
+                f'{results[MISSING_KEYWORD]},'  # current number of missing values
+                f'{results[OUT_OF_RANGE_KEYWORD]},'  # current number of values out of range
+                f'{timestamp}'  # timestamp that the message was emitted
                 f'\n')
 
 
 data_topic = app.topic(kafka_topic)
+results = app.Table('DQ_results', default=int, partitions=1)
 
 
 @app.agent(data_topic, sink=[write_to_csv])
 async def monitor_stream(messages):
-    missing_values = out_of_range_values = 0
     async for message in messages:
         value = message['value']
 
@@ -39,13 +42,9 @@ async def monitor_stream(messages):
         is_out_of_range = range_validator(value)
 
         if is_missing:
-            missing_values += 1
+            results[MISSING_KEYWORD] += 1
         if is_out_of_range:
-            out_of_range_values += 1
+            results[OUT_OF_RANGE_KEYWORD] += 1
 
         if is_missing or is_out_of_range:
-            yield (
-                missing_values,
-                out_of_range_values,
-                standardize_timestamp_to_nanoseconds(time.time())
-            )
+            yield standardize_timestamp_to_nanoseconds(time.time())
