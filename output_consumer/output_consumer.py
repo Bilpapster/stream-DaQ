613ba96da75753e9b98a87e7a12ede0ccb9be485
Vasilis Papastergios <papster@csd.auth.gr>
2025-02-11 21:06:30 +0000

    Write output consumer's results in data/ directory

--- output_consumer/output_consumer.py
+++ output_consumer/output_consumer.py
@@ -12,14 +12,10 @@ SLIDE_DURATION_STR = os.getenv('SLIDE_DURATION', '10 seconds')
 GAP_DURATION_STR = os.getenv('GAP_DURATION', '5 seconds')
 WINDOW_TYPE = os.getenv('WINDOW_TYPE', 'tumbling').lower()
 NUM_CORES = os.getenv('SPARK_NUM_CORES', '20')
-CSV_OUTPUT_PATH = get_output_file_name(WINDOW_TYPE, WINDOW_DURATION_STR, SLIDE_DURATION_STR, GAP_DURATION_STR, NUM_CORES)  # Path inside the container
 
-print(f"Starting output consumer. Listening to topic: {OUTPUT_TOPIC}")
-print(f"Kafka bootstrap servers: {KAFKA_BOOTSTRAP_SERVERS}")
-print(f"Output CSV path: {CSV_OUTPUT_PATH}")
 
 def get_output_file_name(window_type: str, window_duration: str, slide_duration: str | None, gap_duration: str | None, num_cores: str | int) -> str:
-    window_type = str(window_type).to_lower
+    window_type = str(window_type).lower()
     match window_type:
         case "tumbling":
             return f"daq_{window_type}_{window_duration}_{num_cores} cores.csv"
@@ -30,6 +26,12 @@ def get_output_file_name(window_type: str, window_duration: str, slide_duration:
         case _:
             return f"daq_{window_type}_{window_duration}_{slide_duration}_{gap_duration}_{num_cores} cores.csv"
 
+CSV_OUTPUT_PATH = "data/" + get_output_file_name(WINDOW_TYPE, WINDOW_DURATION_STR, SLIDE_DURATION_STR, GAP_DURATION_STR, NUM_CORES)  # Path inside the container
+print(f"Starting output consumer. Listening to topic: {OUTPUT_TOPIC}")
+print(f"Kafka bootstrap servers: {KAFKA_BOOTSTRAP_SERVERS}")
+print(f"Output CSV path: {CSV_OUTPUT_PATH}")
+
+
 def main():
     # Create Kafka consumer
     consumer = KafkaConsumer(
@@ -38,7 +40,7 @@ def main():
         auto_offset_reset='earliest',
         enable_auto_commit=True,
         value_deserializer=lambda m: json.loads(m.decode('utf-8')),
-        consumer_timeout_ms=10*60*1000  # Adjust as needed
+        consumer_timeout_ms=12*60*1000  # Adjust as needed
     )
 
     fieldnames = None
