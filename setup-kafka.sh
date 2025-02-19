8760863650634ff29234e6420f1d1a6f5aeaaa4a
Vasileios Papastergios <papastva@csd.auth.gr>
2025-02-19 13:08:22 +0200

    Add a small sleep period between commands to avoid 'kafka not ready' warnings

--- setup-kafka.sh
+++ setup-kafka.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker compose up -d kafka && docker exec kafka bash /app/scripts/kafka-entrypoint.sh
+docker compose up -d kafka && sleep 5 && docker exec kafka bash /app/scripts/kafka-entrypoint.sh
